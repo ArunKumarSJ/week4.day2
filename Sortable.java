ef986cae966872f7138fb0bd90fcaefdb0939c83
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-09-01 23:06:53 +0530

    Day 8 Assignment

---
+++ Sortable.java
@@ -0,0 +1,30 @@
+package Week4.Day2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sortable {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/sortable.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement SRC = driver.findElementByXPath("//ul[@class='ui-sortable']/li[2]");
+		WebElement DST = driver.findElementByXPath("(//span[@class='ui-icon ui-icon-arrowthick-2-n-s'])[5]");
+		Point location = DST.getLocation();
+		int x = location.getX();
+		int y = location.getY();
+		Actions builder=new Actions(driver);
+		builder.dragAndDropBy(SRC, x, y).perform();
+
+	}
+
+}
