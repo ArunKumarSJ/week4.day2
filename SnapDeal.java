a74cbf325981d0275fa79c7b9f94b272e3079182
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-09-02 16:36:02 +0530

    Day 8 Assignment

--- SnapDeal.java
+++ SnapDeal.java
@@ -3,8 +3,11 @@ package Week4.Day2;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
@@ -27,7 +30,7 @@ public class SnapDeal {
 		Actions builder = new Actions(driver);
 		builder.moveToElement(men).perform();
 		driver.findElementByXPath("(//span[text()='Sports Shoes'])[1]").click();
-	//	Thread.sleep(20000);
+		// Thread.sleep(20000);
 		String SHOESCOUNT = driver.findElementByXPath("(//span[@class='category-count'])[1]").getText();
 		System.out.println("COUNT OF SPORTS SHOES:" + SHOESCOUNT);
 		driver.findElementByXPath("//div[text()='Training Shoes']").click();
@@ -35,9 +38,24 @@ public class SnapDeal {
 		driver.findElementByXPath("//span[text()='Sort by:']").click();
 		Thread.sleep(20000);
 		driver.findElementByXPath("//ul[@class='sort-value']//li[2]").click();
-		String LTOH = driver.findElementByXPath("//div[@class='sort-selected']").getText();
-		System.out.println("THE SORTED LIST IS:" + LTOH);
-	//	Thread.sleep(20000);
+		// String LTOH =
+		// driver.findElementByXPath("//div[@class='sort-selected']").getText();
+		// System.out.println("THE SORTED LIST IS:" + LTOH);
+		Thread.sleep(20000);
+		List<WebElement> PR = driver.findElementsByXPath("//span[@class='lfloat product-price']");
+		Set<String> PRL = new TreeSet<String>();
+		for (int i = 0; i < PR.size(); i++) {
+			PRL.add(PR.get(i).getText());
+
+		}
+		Set<String> PRL1 = new TreeSet<String>(PRL);
+		System.out.println(PRL1);
+		if (PRL1.equals(PRL)) {
+			System.out.println("SORTED");
+		} else {
+			System.out.println("NOT SORTED");
+		}
+
 		JavascriptExecutor kse = (JavascriptExecutor) driver;
 		kse.executeScript("window.scrollBy(0,400)");
 		driver.findElementByXPath("//label[@for='Color_s-Blue']").click();
@@ -66,7 +84,7 @@ public class SnapDeal {
 		driver.findElementByXPath("//label[@for='Brand-VSS']").click();
 		Thread.sleep(10000);
 		String VSS = driver.findElementByXPath("//span[text()='(21 Items)']").getText();
-		System.out.println("COUNT OF VSS BRAND SHOE IS :"+VSS);
+		System.out.println("COUNT OF VSS BRAND SHOE IS :" + VSS);
 	}
 
 }
